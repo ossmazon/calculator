e8e7e39ba5044265cd710aeb0c1f8d6faf487b96
Osvin Mazon Guzman <sergio.mazon@hotmail.com>
2024-07-10 14:04:24 -0600

    added new button

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+//123142
  /* If you want to start measuring performance in your app, pass a function asdasd12312*/
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
